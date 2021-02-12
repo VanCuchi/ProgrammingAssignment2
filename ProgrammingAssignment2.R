afb9c60319404dc991218a14e192527a260024cf
Alec <atalamas@outlook.com>
2021-02-11 22:57:03 -0600

    Second assignment containing makeCacheMatrix and cacheSolve functions

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,50 @@
+
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+n2 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
+#class(m1)
+
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+
+makeCacheMatrix <- function(x = array()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+#above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+#should retrieve the inverse from the cache.
+
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+
+myMatrix_object <- makeCacheMatrix(m1)
+cacheSolve(myMatrix_object)
+cacheSolve(myMatrix_object)
+
+
+myMatrix_object$set(n2)
+cacheSolve(myMatrix_object)
+cacheSolve(myMatrix_object)
+
